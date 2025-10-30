e0f2109d7909aafc36af3973bc541b4fbc43f7b4
hugozeballos <Hzeballosmeneses@gmail.com>
2025-10-30 11:01:42 -0400

    debug page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,9 @@ import Hero from '../components/Hero';
 import Spotlight from '../components/Spotlight';
 import Spotlight_2 from '../components/Spotlight_2';
 import Spotlight_3 from '../components/Spotlight_3';
-import Spotlight_4 from '../components/Spotlight_4';
 import Footer from '../components/Footer';
 import Test from '../components/Test';
 
-import Features from '../components/Features';
 
 export default function Page() {
   return (
